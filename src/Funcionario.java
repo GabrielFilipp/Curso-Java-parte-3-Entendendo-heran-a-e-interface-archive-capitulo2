2f7779fafec56d921dab445a5cb3de6c4320a600
Filipp <filipp278@gmail.com>
2022-04-09 11:35:26 -0300

    aplicando conceito de Abstracao

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,17 +1,15 @@
 
-public class Funcionario {
+public abstract class Funcionario {
 
 	private String nome;
 	private String cpf;
 	private double salario;
-	
+
 	public Funcionario() {
-		
+
 	}
 
-	public double getBonificacao() {
-		return this.salario * 0.05;
-	}
+	public abstract double getBonificacao();
 
 	public String getNome() {
 		return nome;
--- src/TesteFuncionario.java
+++ src/TesteFuncionario.java
@@ -3,14 +3,14 @@ public class TesteFuncionario {
 
 	public static void main(String[] args) {
 
-		Funcionario nico = new Funcionario();
+		Gerente nico = new Gerente();
 		nico.setNome("Nico Steppat");
 		nico.setCpf("223355646-40");
 		nico.setSalario(2600.00);
-		
+
 		System.out.println(nico.getNome());
 		System.out.println(nico.getBonificacao());
-		
+
 	}
 
 }
